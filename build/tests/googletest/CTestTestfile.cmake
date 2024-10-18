ac357e453689cb0bace736fecfd331b2ad05cb93
Navid307 <navid.h307@gmail.com>
2024-10-17 22:45:42 -0400

    Deleted build directory

--- build/tests/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/navid/Github/raspberry_pi_monorepo/dashboard/libs/googletest
-# Build directory: /home/navid/Github/raspberry_pi_monorepo/dashboard/build/tests/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
